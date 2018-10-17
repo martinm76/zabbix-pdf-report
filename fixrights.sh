2255b2a276d65d18b5e9b8a17d694849c5e86d8e
martinm76 <martin.moller@gmail.com>
2018-10-17 15:51:37 +0200

    Modified to create tmp and reports directories if they are missing.

--- fixrights.sh
+++ fixrights.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+if [ ! -e tmp ] ; then 
+  mkdir tmp
+fi
+
+if [ ! -e reports ] ; then
+  mkdir reports
+fi
+
 chmod 777 tmp reports
+
+ls -ld tmp reports
+echo "You should now have tmp and reports folders that your webserver can write to."
