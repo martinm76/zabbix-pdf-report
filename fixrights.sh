2c1cca4d9bb4e2dd725690a3937dcaeed6d968c7
Martin Moller <martin.moller@pwc.com>
2020-07-05 23:47:32 +0200

    Updated fixrights.sh to actually allow reports and tmp to be written to with SELinux enabled.

--- fixrights.sh
+++ fixrights.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ $UID -ne 0 ] ; then
+  echo "You must run this script as root."
+  exit 1
+fi
+
 wwwproc=$(netstat -tpln | grep -e :80 -e :443 | grep / | tail -1 | cut -c80- | cut -d"/" -f1) # PID of process running the webserver
 wwwsubproc=$(pstree -p $wwwproc | tail -1 | cut -d"(" -f2 | cut -d")" -f1) # Find a child process, if possible
 wwwuser=$(ps u $wwwsubproc | tail -1 | cut -d" " -f1) # Hopefully the user running apache
@@ -27,7 +32,16 @@ ls -ld tmp reports
 if [ "$(which getenforce)" != "" ] ; then
 	echo ""
 	echo "SELinux seems to be installed. We will try to fix context. It may not be enough ..."
-	restorecon -vvR $(pwd)
+
+        # SELinux serve files off Apache, resursive
+        chcon -t httpd_sys_content_t $(pwd) -R
+ 
+        # Fix anything SELinux might be cross about
+  	restorecon -vvR $(pwd)
+
+        # Allow write only to specific dirs
+        chcon -t httpd_sys_rw_content_t $(pwd)/reports -R
+        chcon -t httpd_sys_rw_content_t $(pwd)/tmp -R
 fi
 
 echo ""
